f04c941d261ef7dbc1f18fc16ad40058bacab6ca
delbaze <56109313+delbaze@users.noreply.github.com>
2023-04-14 12:36:24 +0200

    passage postgres

--- server/src/lib/datasource.ts
+++ server/src/lib/datasource.ts
@@ -3,11 +3,21 @@ import WilderEntity from "../entity/Wilder.entity";
 import NoteEntity from "../entity/Note.entity";
 import LanguageEntity from "../entity/Language.entity";
 
-
+// export default new DataSource({
+//   type: "sqlite",
+//   database: "./wildersdb.sqlite",
+//   synchronize: true,
+//   entities: [WilderEntity, LanguageEntity, NoteEntity],
+//   logging: ["query", "error"],
+// });
 export default new DataSource({
-  type: "sqlite",
-  database: "./wildersdb.sqlite",
-  synchronize: true,
+  type: "postgres",
+  host: "db",
+  port: 5432,
+  username: "postgres",
+  password: "postgres",
+  database: "wilders",
   entities: [WilderEntity, LanguageEntity, NoteEntity],
-  logging: ["query", "error"],
+  synchronize: true,
+  logging: false,
 });
