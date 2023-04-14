bc497f0fc8b344dec7dc40031cbfd7c2137ae986
delbaze <56109313+delbaze@users.noreply.github.com>
2023-04-14 10:28:35 +0200

    cors et env

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,7 +7,18 @@ import WilderResolver from "./resolvers/wilder.resolver";
 import { ApolloServer } from "@apollo/server";
 import { buildSchema } from "type-graphql";
 import { startStandaloneServer } from "@apollo/server/standalone";
+
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import express from "express";
+import http from "http";
+
 import "reflect-metadata";
+import cors from "cors";
+import { json } from "body-parser";
+
+const app = express();
+const httpServer = http.createServer(app);
 
 const start = async () => {
   await datasource.initialize();
@@ -18,11 +29,28 @@ const start = async () => {
 
   const server = new ApolloServer({
     schema,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
-  console.log(`🚀  Server ready at: ${url}`);
+  // const { url } = await startStandaloneServer(server, {
+  //   listen: { port: 4000 },
+  // });
+  // console.log(`🚀  Server ready at: ${url}`);
+
+  await server.start();
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>({
+      origin: ["http://localhost:3000", "https://studio.apollographql.com"],
+    }),
+    json(),
+    expressMiddleware(server)
+  );
+
+  await new Promise<void>((resolve) =>
+    httpServer.listen({ port: 4000 }, resolve)
+  );
+  console.log(`🚀 Server ready at http://localhost:4000/graphql`);
 };
 
 start();
